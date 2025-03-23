0d96a7e85bbd24fdf519cb6a5a4bad53688dca95
Iyan Zuli Armanda <catcode0101@gmail.com>
2025-03-23 21:00:23 +0700

    feat: SEO sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+  site: 'https://iyan-zuli-armanda.netfiy.app',
 
-  integrations: [react()]
+  integrations: [react(), sitemap()]
 });
